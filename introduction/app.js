ed2beea3b99db7b59246fbe00d439dac591e00d5
harinath0608 <harinath192006@gmail.com>
2026-06-30 08:11:36 +0530

    data types & variables

--- introduction/app.js
+++ introduction/app.js
@@ -1,18 +1,95 @@
-let ChangeText = document.querySelectorAll(".style")
-let btn = document.getElementById("btn")
+// let ChangeText = document.querySelectorAll(".style")
+// let btn = document.getElementById("btn")
 
-console.log(ChangeText[0])
-console.log(ChangeText[1])
-console.log(ChangeText[2])
+// console.log(ChangeText[0])
+// console.log(ChangeText[1])
+// console.log(ChangeText[2])
 
 
-function changeHeading() {
+// function changeHeading() {
 
-    ChangeText.textContent = "Javascript is the Dynamic Language"
-    ChangeText[0].style.color = "blue"
-    ChangeText[1].style.color = "blue"
-     ChangeText.style.backgroundColor = "red"
+//     ChangeText.textContent = "20"
+//     ChangeText[0].style.color = "blue"
+//     ChangeText[1].style.color = "blue"
+//      ChangeText.style.backgroundColor = "red"
 
-}
+// }
+
+
+// let bottle = "water"
+
+// console.log(bottle)
+
+// bottle = "Milk"
+
+// console.log(bottle)
+
+// redeclaration is possible , reassignment is possible
+
+
+// const fruit = "apple"
+
+// console.log(fruit);
+
+// fruit = "banana"
+
+// console.log(fruit);
+
+
+// var a = 10 
+
+// var a = 20 
+
+// var b = 50 
+
+// var c = a+b
+
+// c = a = b 
+
+// console.log(c)
+
+
+// let output = document.getElementById("value")
+
+// output.textContent = 20 
+
+
+// variable can assign any number of value 
+
+
+// let values = [1,2,"apple", true]
+
+// console.log(values);
+
+
+// let a = "hari"
+
+// a = 20
+
+// a = true
+
+// a = null
+
+// a = 20 
+
+// c = a+b
+
+// console.log(typeof c);
+
+
+// a = undefined 
+
+// console.log(a);
+
+
+// console.log("Hello")
+// console.error("Error Found")
+// console.warn("Warning")
+
+
+
+
+
+
 
 
