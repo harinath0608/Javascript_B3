3ca142079992295fd2b2012f24cbb09b582c09dd
harinath0608 <harinath192006@gmail.com>
2026-07-03 07:51:15 +0530

    loops

---
+++ Loops/app.js
@@ -0,0 +1,64 @@
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+
+
+// for (initialation , logic, update ) {
+//     // logic 
+// }
+
+// 1 to 10 
+
+// for (let i = 1 ; i<=10 ; i++) {
+
+//     console.log(i)
+
+// }
+
+// debugger
+
+// for (let i = 10; i > 0 ; i--) {
+//     console.log(i)
+// }
+
+// for (let i = 3 ; i <=20 ; i += 3) {
+//     console.log("Even Numbers:" , i);  
+// }
+
+
+// debugger
+
+// let i = 1 
+
+// while (i<=5) {
+
+//     console.log(i);
+
+//     i++
+    
+// }
+
+// debugger
+
+// let i = 1 
+
+// do {
+
+//     console.log(i)
+
+//     i++
+
+// } while (i<=5) {
+
+// }
+
+for (let i = 1 ; i<=10 ; i++ ) {
+
+    if( i === 12){
+        break
+    }
+
+    console.log(i);
+    
+}
---
+++ Operators/app.js
@@ -0,0 +1,98 @@
+
+
+// let c = a+b
+
+// let d = a-b
+
+// let e = c*d
+
+// let f = a/b
+
+// // console.log(c)
+// // console.log(d)
+// // console.log(e)
+
+// console.log(f);
+
+
+// let mobilePrice = 15000;
+// let quantity = 2;
+// let totalPrice = mobilePrice * quantity;
+// console.log(totalPrice);
+// let a = 60
+// let b = 60 
+
+// console.log(a >= b)
+
+// console.log(a <= b)
+
+// console.log(a==b)
+
+// == - loose equal to can checks the values of left and right 
+
+// console.log(a === b)
+
+// === - strict equal to checks the data type and value as well - true and false - false 
+
+
+
+// let age = prompt()
+
+// if (age<18) {
+//     console.log("Not eligible to vote")
+// }
+
+// else if (age>18) {
+//     console.log("Eligible to vote")
+// }
+
+
+// let age = 18
+
+// console.log(age<=18 && age < 60)
+
+
+// let marks = prompt()
+
+// if ( marks > 90 && marks >= 90)   {
+//     console.log("A Grade")
+// }
+
+// else if (marks > 75 && marks >= 75) {
+//     console.log("B Grade");
+// }
+
+// else if (marks >= 50) {
+//     console.log("C Grade")
+// }
+
+// else if (marks <= 50) {
+//     console.log("Fail");
+// }
+
+// let age = 25
+
+// if (age >= 18 || age >= 60) {
+//     console.log("Eligible to vote"); 
+// }
+
+
+// let day = 4;
+// switch(day){
+//  case 1:
+//  console.log("Monday");
+//  break;
+//  case 2:
+//  console.log("Tuesday");
+//  break;
+//  case 3:
+//  console.log("Wednesday");
+//  break;
+//  default:
+//  console.log("Invalid Day");
+// }
+
+
+
+
+
