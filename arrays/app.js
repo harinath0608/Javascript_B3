00aff3bacaae2674ef322e4e688d23e4075d0067
harinath0608 <harinath192006@gmail.com>
2026-07-09 07:52:41 +0530

    arrays& methods

---
+++ arrays/app.js
@@ -0,0 +1,29 @@
+const fruits = ["apple", "orange", "banana", 3, true, 1.4]
+
+// fruits.push("Kiwi")
+
+// console.log(fruits);
+
+// fruits.pop()
+
+// console.log(fruits);
+
+// fruits.pop()
+
+// console.log(fruits);
+
+
+// fruits.shift("Water")
+
+// console.log(fruits);
+
+// fruits.unshift("water")
+
+// console.log(fruits);
+
+
+let result = fruits.slice(2,6)
+
+console.log(result);
+
+
